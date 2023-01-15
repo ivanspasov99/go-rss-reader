9723a57c74f984795a9f159571fcc5e6298ebdfa
ivanspasov99 <johnspasov99@gmail.com>
2023-01-15 19:25:34 +0200

    example main

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/ivanspasov99/rss-reader/pkg/meta"
+	"github.com/ivanspasov99/rss-reader/pkg/rss"
+)
+
+func ParseMock(urls []string) []rss.RssItem {
+	return []rss.RssItem{
+		{
+			Title:       "sub1-title",
+			Source:      "main-title",
+			SourceURL:   "main-link",
+			Description: "sub1-desc",
+			Link:        "sub1-link",
+		},
+		{
+			Title:       "sub2-title",
+			Source:      "main-title",
+			SourceURL:   "main-link",
+			Description: "sub2-desc",
+			Link:        "sub2-link",
+		},
+	}
+}
 
 func main() {
-	fmt.Println("Hello")
+	if err := meta.ParseFeedAsJSON(nil, "./rss.json", ParseMock); err != nil {
+		fmt.Println(err)
+		return
+	}
 }
